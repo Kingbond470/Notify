98ac864693851db2aa9f3fd80d8a1dd7fc2d061b
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-19 17:14:30 +0530

    Home Layout

---
+++ app/src/main/java/dev/kingbond/notify/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package dev.kingbond.notify.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.kingbond.notify.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
