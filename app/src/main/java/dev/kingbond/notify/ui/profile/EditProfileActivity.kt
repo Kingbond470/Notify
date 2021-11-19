14c1eda8abfbac69db29f759d3da2ddc91012246
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-19 22:50:38 +0530

    Edit Profile | Started

---
+++ app/src/main/java/dev/kingbond/notify/ui/profile/EditProfileActivity.kt
@@ -0,0 +1,12 @@
+package dev.kingbond.notify.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.kingbond.notify.R
+
+class EditProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile)
+    }
+}
