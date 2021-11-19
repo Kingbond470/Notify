4384478dda877b59774e9b5813243096509b159a
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-20 00:59:14 +0530

    Setting  - User Layout | done

---
+++ app/src/main/java/dev/kingbond/notify/ui/settings/SettingsUserProfileActivity.kt
@@ -0,0 +1,12 @@
+package dev.kingbond.notify.ui.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.kingbond.notify.R
+
+class SettingsUserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_user_profile)
+    }
+}
