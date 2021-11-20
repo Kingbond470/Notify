2c61d404be79e029e9a475dca311c7b5e0f1f398
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-20 11:27:00 +0530

    Setting | Bottom Navigation

--- app/src/main/java/dev/kingbond/notify/ui/settings/SettingFragment.kt
+++ app/src/main/java/dev/kingbond/notify/ui/settings/SettingFragment.kt
@@ -1,60 +1,123 @@
 package dev.kingbond.notify.ui.settings
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Environment
+import android.os.StatFs
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import androidx.databinding.DataBindingUtil.setContentView
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import dev.kingbond.notify.R
+import dev.kingbond.notify.databinding.ActivitySettingsBinding
+import dev.kingbond.notify.databinding.FragmentSettingBinding
+import dev.kingbond.notify.ui.home.HomeActivity
+import dev.kingbond.notify.ui.login.LoginActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SettingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SettingFragment
+    : Fragment(R.layout.fragment_setting) {
+    private lateinit var settingsBinding: FragmentSettingBinding
+    //profile
+    private lateinit var mAuth: FirebaseAuth
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
-    }
+            settingsBinding = FragmentSettingBinding.bind(view)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SettingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+            settingsBinding.llViewProfile.setOnClickListener {
+               // startActivity(Intent(requireContext(), SettingsUserProfileActivity::class.java))
+
             }
+
+
+            //profile
+            mAuth = FirebaseAuth.getInstance()
+            val user = mAuth.currentUser
+            if (user != null) {
+                Glide.with(settingsBinding.ivSettingProfileImage).load(user.photoUrl)
+                    .into(settingsBinding.ivSettingProfileImage)
+                settingsBinding.tvSettingsProfileName.text = user.displayName
+                settingsBinding.ivEmail.text = user.email
+                settingsBinding.tvLogOutName.text = "You are logged in as ${user.displayName}"
+                Glide.with(settingsBinding.ivSettingProfileImage).load(user.photoUrl)
+                    .into(settingsBinding.ivSettingProfileImage)
+                settingsBinding.tvSettingsProfileName.text = user.displayName
+                settingsBinding.ivEmail.text = user.email
+                settingsBinding.tvLogOutName.text = "You are logged in as ${user.displayName}"
+            } else {
+                Glide.with(settingsBinding.ivSettingProfileImage).load(R.drawable.man)
+                    .into(settingsBinding.ivSettingProfileImage)
+                settingsBinding.tvSettingsProfileName.text = "Masai Android"
+            }
+
+            // back to mainactivity
+//        cdSettingsProfile.setOnClickListener {
+//            val intent = Intent(context, MainActivity::class.java)
+//            startActivity(intent)
+//        }
+
+            //logout
+            settingsBinding.tvLogOut.setOnClickListener {
+                mAuth.signOut()
+                val logout = Intent(requireContext(), LoginActivity::class.java)
+                startActivity(logout)
+
+                //  (activity as Activity?)?.overridePendingTransition(0, 0)
+            }
+
+            if (settingsBinding.spinnerSettingDownload.count > 1) {
+                settingsBinding.spinnerSettingDownload.setSelection(1)
+            }
+
+            settingsBinding.seekbarSettingCrossfade.setOnSeekBarChangeListener(object :
+                SeekBar.OnSeekBarChangeListener {
+                override fun onStopTrackingTouch(seekBar: SeekBar) {}
+                override fun onStartTrackingTouch(seekBar: SeekBar) {}
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    if (fromUser) {
+                        if (progress >= 0 && progress <= seekBar.max) {
+                            val progressString = (progress).toString() + " s"
+                            settingsBinding.tvCrossfade12s.text =
+                                progressString // the TextView Reference
+                            seekBar.secondaryProgress = progress
+                        }
+                    }
+                }
+            })
+
+            //for storage
+            val statFs = StatFs(Environment.getDataDirectory().absolutePath);
+            var total = (statFs.blockCount.toFloat() * statFs.blockSize) / 1048576;
+            var free = (statFs.availableBlocks.toFloat() * statFs.blockSize) / 1048576;
+            total /= 1024
+            free /= 1024
+            total = String.format("%.1f", total).toFloat()
+            free = String.format("%.1f", free).toFloat()
+
+            val busy = String.format("%.1f", (total - free)).toFloat()
+
+            settingsBinding.indicator.max = total.toInt()
+            settingsBinding.indicator.progress = busy.toInt()
+            settingsBinding.tvFreeSpace.text = "$free GB"
+            settingsBinding.tvUsedSpace.text = "$busy GB"
+
+//            val cache = cacheDir.toString()
+//            settingsBinding.tvCacheSpace.text = "2.2 MB"
+//
+//            settingsBinding.llDeleteCache.setOnClickListener {
+//                cacheDir?.deleteRecursively()
+//                settingsBinding.rlCache.visibility = View.GONE
+//            }
+
+        }
+
+
+
     }
-}
+
