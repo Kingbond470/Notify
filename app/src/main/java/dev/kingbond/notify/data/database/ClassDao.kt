9166648bc871fed5e99554045760cff8bb1b3a37
dell <abhimanyumishra130@gmail.com>
2021-11-20 20:26:51 +0530

    goal done!

--- app/src/main/java/dev/kingbond/notify/data/database/ClassDao.kt
+++ app/src/main/java/dev/kingbond/notify/data/database/ClassDao.kt
@@ -1,10 +1,7 @@
 package dev.kingbond.notify.data.database
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import dev.kingbond.notify.ui.goal.model.GoalModel
 import dev.kingbond.notify.ui.task.model.TaskModel
 
@@ -14,15 +11,23 @@ interface ClassDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertDataInGoal(goalModel: GoalModel)
 
+    @Update
+    fun updateDataInGoal(goalModel: GoalModel)
+
     @Query("select * from goal_table")
     fun fetchDataFromGoal():LiveData<List<GoalModel>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertDataInTask(taskModel: TaskModel)
 
+    @Update
+    fun updateDataInTask(taskModel: TaskModel)
+
     @Query("select * from task_table")
     fun fetchDataFromTask():LiveData<List<TaskModel>>
 
     @Query("select * from task_table where category = :goalName")
     fun getTasksWithGoal(goalName:String):LiveData<List<TaskModel>>
+
+
 }
--- app/src/main/java/dev/kingbond/notify/ui/goal/GoalHomeActivity.kt
+++ app/src/main/java/dev/kingbond/notify/ui/goal/GoalHomeActivity.kt
@@ -55,7 +55,7 @@ class GoalHomeActivity : AppCompatActivity(), GoalClickListener {
     }
 
     private fun setRecyclerView() {
-        adapter = GoalAdapter(list,this,itemViewModel)
+        adapter = GoalAdapter(list,this)
         val linearLayoutManager = LinearLayoutManager(this)
         binding.apply {
             goalRecyclerView.adapter = adapter
