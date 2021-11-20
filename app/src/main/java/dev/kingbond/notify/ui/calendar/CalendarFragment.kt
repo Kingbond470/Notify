88535a5184c338346c46ba80ff418d59e64d680e
Mausam Singh <53150785+Kingbond470@users.noreply.github.com>
2021-11-20 18:30:08 +0530

    Layout fix

--- app/src/main/java/dev/kingbond/notify/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/dev/kingbond/notify/ui/calendar/CalendarFragment.kt
@@ -8,26 +8,80 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dev.kingbond.notify.R
+import dev.kingbond.notify.data.database.RoomDataBaseClass
+import dev.kingbond.notify.databinding.ActivityGoalDetailsBinding
+import dev.kingbond.notify.databinding.FragmentCalendarBinding
+import dev.kingbond.notify.repository.RepositoryClass
+import dev.kingbond.notify.ui.goal.model.GoalModel
+import dev.kingbond.notify.ui.goal.recyclerView.GoalAdapter
+import dev.kingbond.notify.ui.goal.recyclerView.GoalClickListener
+import dev.kingbond.notify.ui.task.model.TaskModel
+import dev.kingbond.notify.ui.task.recyclerView.TaskAdapter
+import dev.kingbond.notify.viewmodel.ViewModelClass
+import dev.kingbond.notify.viewmodel.ViewModelFactory
 import kotlinx.android.synthetic.main.fragment_calendar.*
 import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 
 @RequiresApi(Build.VERSION_CODES.O)
-class CalendarFragment : Fragment(R.layout.fragment_calendar), DateClickListener {
+class CalendarFragment : Fragment(R.layout.fragment_calendar), DateClickListener, GoalClickListener {
 
     private var selectedDate: LocalDate? = null
     private lateinit var calendarAdapter: CalendarAdapter
 
+    private lateinit var binding: FragmentCalendarBinding
+    private lateinit var itemViewModel: ViewModelClass
+    private lateinit var goalAdapter: GoalAdapter
+    private var listGoal = arrayListOf<GoalModel>()
+    private lateinit var daysInMonth: ArrayList<String>
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentCalendarBinding.bind(view)
+
+        val roomDatabase = RoomDataBaseClass.getDataBaseObject(requireContext())
+        val dao = roomDatabase.getDao()
+        val repo = RepositoryClass(dao)
+        val viewModelFactory = ViewModelFactory(repo)
+        itemViewModel =
+            ViewModelProviders.of(this, viewModelFactory).get(ViewModelClass::class.java)
+
+        rcvGoals()
 
         selectedDate = LocalDate.now()
         setMonthView()
         jumpToDate()
+    }
+
+    private fun rcvGoals() {
+        itemViewModel.getDataFromGoal().observe(viewLifecycleOwner, Observer {
+//            listGoal.clear()
+            listGoal.addAll(it)
+            listGoal.addAll(it)
+            listGoal.addAll(it)
+            listGoal.addAll(it)
+            goalAdapter.notifyDataSetChanged()
+        })
+
+        goalAdapter = GoalAdapter(listGoal, this)
+        val linearLayoutManager = LinearLayoutManager(requireContext())
+        val linearLayoutManager2 = LinearLayoutManager(requireContext())
+        binding.apply {
+            rcvGoalsCalendar.adapter = goalAdapter
+            rcvGoalsCalendar.layoutManager = linearLayoutManager
+            rcvGoalsCalendar.isNestedScrollingEnabled = false
+            rcvGoalsCalendar2.adapter = goalAdapter
+            rcvGoalsCalendar2.layoutManager = linearLayoutManager2
+            rcvGoalsCalendar2.isNestedScrollingEnabled = false
+        }
     }
 
     private fun jumpToDate() {
@@ -40,12 +94,15 @@ class CalendarFragment : Fragment(R.layout.fragment_calendar), DateClickListener
         nextMonthAction(view)
 
         val currentDate = selectedDate.toString()
-        monthYearTV.text = monthYearFromDate(selectedDate!!)
-        val daysInMonth: ArrayList<String> = daysInMonthArray(selectedDate!!)
+        daysInMonth = daysInMonthArray(selectedDate!!)
         calendarAdapter = CalendarAdapter(daysInMonth, this, currentDate)
         val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(requireContext(), 7)
-        calendarRecyclerView.layoutManager = layoutManager
-        calendarRecyclerView.adapter = calendarAdapter
+
+        binding.apply {
+            monthYearTV.text = monthYearFromDate(selectedDate!!)
+            calendarRecyclerView.layoutManager = layoutManager
+            calendarRecyclerView.adapter = calendarAdapter
+        }
 //        currentdate.text = selectedDate.toString()
     }
 
@@ -59,11 +116,6 @@ class CalendarFragment : Fragment(R.layout.fragment_calendar), DateClickListener
             if (i <= dayOfWeek || i > daysInMonth + dayOfWeek) {
                 if (i > daysInMonth + dayOfWeek) break
                 daysInMonthArray.add("")
-//                if(i > daysInMonth + dayOfWeek) {
-//                    daysInMonthArray.add(((daysInMonth + dayOfWeek)%daysInMonth).toString())
-//                } else {
-//                    daysInMonthArray.add((daysInMonth + dayOfWeek).toString())
-//                }
             } else {
                 daysInMonthArray.add((i - dayOfWeek).toString())
             }
@@ -77,14 +129,14 @@ class CalendarFragment : Fragment(R.layout.fragment_calendar), DateClickListener
     }
 
     private fun previousMonthAction(view: View?) {
-        previousMonth.setOnClickListener {
+        binding.previousMonth.setOnClickListener {
             selectedDate = selectedDate!!.minusMonths(1)
             setMonthView()
         }
     }
 
     private fun nextMonthAction(view: View?) {
-        nextMonth.setOnClickListener {
+        binding.nextMonth.setOnClickListener {
             selectedDate = selectedDate!!.plusMonths(1)
             setMonthView()
         }
@@ -101,13 +153,21 @@ class CalendarFragment : Fragment(R.layout.fragment_calendar), DateClickListener
 //    }
 
     override fun onDateClicked(date: String, position: Int) {
-        if (!date.equals("")) {
+        if (date != "") {
             val message =
                 "Selected Date " + date.toString() + " " + monthYearFromDate(selectedDate!!)
             Toast.makeText(context, message, Toast.LENGTH_LONG).show()
         } else {
             Toast.makeText(context, "null", Toast.LENGTH_LONG).show()
         }
+    }
+
+    override fun goalItemClicked(goalModel: GoalModel) {
+        TODO("Not yet implemented")
+    }
+
+    override fun goalItemAddClicked(goalModel: GoalModel) {
+        TODO("Not yet implemented")
     }
 
 }
